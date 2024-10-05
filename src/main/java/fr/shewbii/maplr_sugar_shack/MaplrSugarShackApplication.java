5374aa29726dd6b6e8a40094b9025c08c3ae30c2
Shewbii <shewbii@free.fr>
2024-10-05 09:06:42 +0200

    First commit

---
+++ src/main/java/fr/shewbii/maplr_sugar_shack/MaplrSugarShackApplication.java
@@ -0,0 +1,13 @@
+package fr.shewbii.maplr_sugar_shack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplrSugarShackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaplrSugarShackApplication.class, args);
+	}
+
+}
